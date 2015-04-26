f8a8f84f9dd9163a2a15561c25c81f95ea2a3893
aburan28 <aburan28@gmail.com>
2015-04-26 14:15:39 +0000

    Initial commit

---
+++ hashfunctions.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+__author__ = "Adam Buran"
+__version__ = ""
+__contact__ = "aburan28@gmail.com"
+
+
+
+
+import smhasher
+
+def generate_hashfunctions(nbr_bits, nbr_slices):
+    """Generate a set of hash functions.
+    The core method is 32-bits murmur3 hash which have good distribution properties
+    Multiple hashes can be generate using the previous hash value as a seed.
+    """
+    def _make_hashfuncs(key):
+        if isinstance(key, unicode):
+            key = key.encode('utf-8')
+        else:
+            key = str(key)
+        rval = []
+        current_hash = None
+        for i in range(nbr_slices):
+            seed = current_hash or 0
+            current_hash = smhasher.murmur3_x86_64(key, seed)
+            rval.append(current_hash % nbr_bits)
+        return rval
+    return _make_hashfuncs
+
+def get_raw_hashfunctions():
+    return utils.murmurhash3_32
